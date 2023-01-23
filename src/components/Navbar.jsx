16e478a45b964b68214a06e555ec13efecf204cf
Generalsoi <ikhinobelesuccess@gmail.com>
2023-01-23 21:57:24 +0100

    fix: make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { GrClose } from "react-icons/gr";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   let navLinks = [
     { name: "About", link: "/" },
     { name: "Experience", link: "/" },
@@ -8,9 +12,14 @@ export const Navbar = () => {
     { name: "Contact", link: "/" },
   ];
 
+  //handle click function for the hamburger
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className="w-[100%]">
-      <div className="w-full md:px-20 py-12 md:flex items-center justify-between">
+    <div className="w-[100%] relative">
+      <div className="w-full px-4 md:px-20 py-8 md:py-12 md:flex items-center justify-between">
         <div>
           <span className="border-2 border-[#64FFDA] p-4 font-poppins">
             <span className="text-[#64FFDA]">Daniel</span>Fayemi
@@ -18,17 +27,32 @@ export const Navbar = () => {
           </span>
         </div>
 
-        <ul className="md:flex items-center gap-8 font-poppins">
+        <span
+          className={`md:hidden text-3xl font-extrabold absolute top-8 right-4 transition-all duration-300 text-white`}
+          onClick={handleClick}
+        >
+          {open ? (
+            <GrClose className="text-white" />
+          ) : (
+            <RxHamburgerMenu className="text-white" />
+          )}
+        </span>
+
+        <ul
+          className={`${
+            open ? "static" : "hidden transition-all ease-in-out duration-300"
+          } md:static md:flex items-center gap-8 font-poppins mt-12 md:mt-0 `}
+        >
           {navLinks.map((navLink, index) => (
             <li
               key={index}
-              className="hover:border-t-0 hover:border-r-0 hover:border-l-0 hover:border-b-4 hover:border-b-[#64FFDA] cursor-pointer transition-all duration-300"
+              className="mt-6 md:mt-0 hover:border-t-0 hover:border-r-0 hover:border-l-0 hover:border-b-4 hover:border-b-[#64FFDA] cursor-pointer transition-all duration-300"
             >
               {navLink.name}
             </li>
           ))}
 
-          <button className="border border-[#64FFDA] rounded-lg py-2 px-4 hover:bg-[#64FFDA] hover:text-white ">
+          <button className="mt-6 md:mt-0 border border-[#64FFDA] rounded-lg py-2 px-4 hover:bg-[#64FFDA] hover:text-white ">
             Resume
           </button>
         </ul>
