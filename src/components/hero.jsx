e0e534db7854119215129d394917336f77b94c91
Generalsoi <ikhinobelesuccess@gmail.com>
2023-01-25 00:14:55 +0100

    fix: implement the hero section of the portfolio

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TypewriterComponent from "typewriter-effect";
 
 export const Hero = () => {
   return (
@@ -6,12 +7,32 @@ export const Hero = () => {
       <h4 className="text-[#64FFDA] md:text-xl font-poppins ">
         Hi, my name is
       </h4>
-      <h1 className="mt-8 font-inter md:text-5xl font-extrabold">
+      <h1 className="mt-5 md:mt-8 font-inter text-3xl md:text-5xl font-extrabold">
         Daniel Fayemi.
       </h1>
-      <h1 className="mt-4 font-inter md:text-4xl font-extrabold text-[#D9D9D9]">
-        I build World-class Software and Solutions.
-      </h1>
+      <div className="mt-4 font-inter text-2xl md:text-4xl font-extrabold text-[#D9D9D9]">
+        <TypewriterComponent
+          onInit={(typewriter) => {
+            typewriter
+
+              .typeString(
+                `
+                I build World-class Software and Solutions.
+              `
+              )
+
+              .start();
+          }}
+        />
+      </div>
+
+      <p className="text-[#A8B2D1] md:w-[60%] text-sm md:text-lg mt-5 font-poppins">
+        I am a Software engineer specializing in building with a wide range of
+        technologies and tools that include working on blockchain, Fin-tech &
+        crypto projects. In recent times I have been researching and building
+        new blockchain/crypto solutions and blockchain integrations with several
+        top blockchain and crypto companies like Binance, Latoken, Chippercash
+      </p>
     </div>
   );
 };
