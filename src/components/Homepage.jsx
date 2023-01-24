22f9677f6e03a41aa14989f27e381016f6a5c416
Generalsoi <ikhinobelesuccess@gmail.com>
2023-01-24 23:20:28 +0100

    fix: begin implementing the hero section of the portfolio

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Hero } from "./hero";
 import { Navbar } from "./Navbar";
 
 export const Homepage = () => {
   return (
     <div className="text-white">
       <Navbar />
+      <Hero />
     </div>
   );
 };
