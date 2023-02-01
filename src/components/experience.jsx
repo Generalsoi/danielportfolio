50c3bdfe1f99ce9dd3db9a8c95590c81228b4f85
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-01 16:15:29 +0100

    fix: add a js file containing json data about daniel"s experience

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { workExperience } from "../helpers/experience";
 
 export const Experience = () => {
   return (
-    <div>
+    <div className="px-4 md:px-28 py-8 md:py-16 font-poppins">
       <div className="flex items-center ">
         <h1 className="md:text-4xl mb-10">
           Where I've worked<span className="text-[#64FFDA]">.</span>
         </h1>
         <div className="h-1 w-[50%] bg-[#A8B2D1]"></div>
+      </div>
+
+      <div>
+        <div>
+          {workExperience.map((experience, id) => (
+            <h3 key={id}>{experience.company}</h3>
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/helpers/experience.js
@@ -0,0 +1,41 @@
+export const workExperience = [
+  {
+    company: "Patricia Universe, Remote",
+    role: "Senior Backend Engineer",
+    roleDescriptions: [
+      "Designed and Built an off-chain scalable solution for Instant Trading (Buying & Selling) of Bitcoin by users using Fiat Digital Wallets.",
+      "Implemented Custodian Wallet for users to enable Bitcoin Transfers (Send & Receive).",
+      "Added value-added services ( Bill Payment, Tickets, Airtime & Internet Subscription ) which can be purchased using bitcoin.",
+      "Integrated Debit cards into off-chain solution to enable spending bitcoin from ATM or online card transactions.",
+    ],
+    timeline: "2016-01 - 2018-02",
+  },
+  {
+    company: "Chipper Cash, Remote",
+    role: "Senior Backend Engineer",
+    roleDescriptions: [
+      "Built Internal Tooling for Admin and Customer support to handle growing user base effectively.",
+      "Integrating New Blockchains and Crypto Tokens which included DOGE, and SOLANA via Fireblocks custodial wallet.",
+      "Debugging legacy code issues and Bug Fixing recurring bugs to achieve backend system maturity.",
+    ],
+    timeline: "2018-07 - 2021-01",
+  },
+  {
+    company: "LaToken, Remote",
+    role: "Senior Backend Engineer",
+    roleDescriptions: [
+      "Researched and Integrated multiple New Blockchains, Cryptocurrencies, and Tokens into Latoken backend, Helium, Near, Cardano, Harmony.",
+      "Integrated Multiple Fiat Deposit and Withdrawal Channels and Gateways on Latoken Platform.",
+    ],
+    timeline: "2021-01 - 2022-02",
+  },
+  {
+    company: "Binance, Remote",
+    role: "Senior Backend Engineer",
+    roleDescriptions: [
+      "Designed and developed Cashlink product that enabled west African users to deposit and withdraw fiat on Binance platform.",
+      "Designed and Built Bundle Socials service that displays news and articles about crypto and blockchain to users.",
+    ],
+    timeline: "2022-02 - 2022-11",
+  },
+];
