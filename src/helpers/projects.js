e0a1f6164ffa7327fccafac6555d1d63ab7eb457
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-05 21:43:49 +0100

    fix: created a project details js file

---
+++ src/helpers/projects.js
@@ -0,0 +1,43 @@
+export const projectDetails = [
+  {
+    id: 1,
+    name: "Crypto Blockchain Provider",
+    githubLink: "https://github.com/WrungCodes/crypto-blockchain-provider",
+    description:
+      "An interface for users to interact with different blockchains. Technologies used include Typescript.",
+  },
+  {
+    id: 2,
+    name: "Block Reader Service",
+    githubLink: "https://github.com/WrungCodes/block-reader-service",
+    description:
+      "An example service on how to read data real time from blockchains. Technologies used include Typescript and Docker.",
+  },
+  {
+    id: 3,
+    name: "Flutechain",
+    githubLink: "https://github.com/WrungCodes/flutechain",
+    description:
+      "An EVM compatible blockchain I built for fun. Major technologies used include Go.",
+  },
+  {
+    id: 4,
+    name: "Order book",
+    githubLink: "https://github.com/WrungCodes/order-book",
+    description: "An order book built using Typescript (WIP)",
+  },
+  {
+    id: 5,
+    name: "Common NPM Package",
+    githubLink: "https://github.com/WrungCodes/common-npm-package",
+    description:
+      "An example common NPM package that can be used for reusability. Built using typescript. ",
+  },
+  {
+    id: 6,
+    name: "Simple Bank",
+    githubLink: "https://github.com/WrungCodes/simplebank",
+    description:
+      "Goland API implemented using SQLC (raw queries). Technologies used include: Golang, Makefile. ",
+  },
+];
