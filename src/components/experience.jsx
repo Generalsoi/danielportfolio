d71ddf70f325f10ed69e7f7209720bc37dd7ebe2
Generalsoi <ikhinobelesuccess@gmail.com>
2023-02-06 00:06:40 +0100

    fix: minor updates

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -10,7 +10,7 @@ export const Experience = () => {
   return (
     <div className="px-4 md:px-28 py-8 md:py-16 font-poppins" id="experience">
       <div className="flex items-center ">
-        <h1 className="md:text-4xl mb-10">
+        <h1 className="md:text-4xl mb-10 font-extrabold">
           Where I've worked<span className="text-[#64FFDA]">.</span>
         </h1>
         <div className="h-1 w-[50%] bg-[#A8B2D1]"></div>
